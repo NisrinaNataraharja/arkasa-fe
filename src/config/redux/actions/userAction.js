811175c20ccc52df03d3ae7ea6e02277a13b3eb9
NisrinaNataraharja <nisrinanataraharja@gmail.com>
2022-07-13 13:04:11 +0700

    integrasi login register

--- src/config/redux/actions/userAction.js
+++ src/config/redux/actions/userAction.js
@@ -4,21 +4,20 @@ import swal from 'sweetalert';
 export const loginUser = (loginData, navigate) => async (dispatch) => {
     try {
         dispatch({ type: 'USER_LOGIN_PENDING' })
-        const result = await axios.post(`${process.env.REACT_APP_API_BACKEND}/v1/users/login`, loginData)
+        const result = await axios.post(`${process.env.REACT_APP_API_BACKEND}/auth/login`, loginData)
         const user = result.data.data
 
         const dataLocal = {
-            firstName: result.data.data.first_name,
-            lastName: result.data.data.last_name,
+            name: result.data.data.name,
             id: result.data.data.id,
             email: result.data.data.email,
             role: result.data.data.role,
             token: result.data.data.token,
-            refreshToken: result.data.data.RefreshToken,
+            refreshToken: result.data.data.refreshToken,
         }
 
         console.log(dataLocal)
-        localStorage.setItem('BlanjaUser', JSON.stringify(dataLocal))
+        localStorage.setItem('Arkasa', JSON.stringify(dataLocal))
 
         dispatch({ type: 'USER_LOGIN_SUCCESS', payload: user })
 
@@ -33,7 +32,27 @@ export const loginUser = (loginData, navigate) => async (dispatch) => {
         console.log(error);
         dispatch({ type: 'USER_LOGIN_ERROR' })
         swal({
-            title: "Good job!",
+            title: "Oops!",
+            text: `${error.response.data.message}`,
+            icon: "error",
+        });
+    }
+}
+
+export const registerUser = (dataForm, navigate)=> async(dispatch)=>{
+    try {
+        dispatch({type: 'USER_REGISTER_PENDING'})
+        const result = await axios.post(`${process.env.REACT_APP_API_BACKEND}/auth/register`, dataForm)
+        const user = result.data
+        console.log(result.data);
+        dispatch({type: 'USER_REGISTER_SUCCESS', payload: user})
+        navigate('/login')
+
+    } catch (error) {
+        console.log(error);
+        dispatch({ type: 'USER_REGISTER_ERROR' })
+        swal({
+            title: "Oops!",
             text: `${error.response.data.message}`,
             icon: "error",
         });
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -5,28 +5,40 @@ import Button from "../../components/base/button";
 import styles from "./register.module.css";
 import LogoBaner from "../../components/base/logobaner";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import swal from "sweetalert";
 import * as yup from "yup";
 import { useFormik } from "formik";
+import {registerUser} from '../../config/redux/actions/userAction'
 
 const Register = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch()
   const formik = useFormik({
     initialValues: {
-      fullname: "",
+      name: "",
       email: "",
       password: "",
     },
     validationSchema: yup.object({
-      fullname: yup.string().required("Required"),
+      name: yup.string().required("Required"),
       email: yup.string().email("Invalid email format").required("Required"),
       password: yup.string().min(8, "Minimum 8 character").required("Required"),
     }),
     onSubmit: (values) => {
-      console.log("form data", formik.values);
-      swal("Register Succes!", "success anda Register", "success");
-      navigate("/login");
-    },
+        try {
+          dispatch(registerUser(values, navigate))
+          
+        } catch (error) {
+          swal.fire({
+            title: "Error!",
+            text: error,
+            icon: "error",
+            confirmButtonText: "Ok",
+            confirmButtonColor: "#6a4029",
+        });
+        }
+      },
   });
   return (
     <div className={styles.pageRegister}>
@@ -40,13 +52,13 @@ const Register = () => {
             <h1>Register</h1>
           </div>
           <form onSubmit={formik.handleSubmit}>
-            <Input type="text" name="fullname" placeholder="Full Name" className="inputLogin" value={formik.values.fullname} onChange={formik.handleChange} />
-            {formik.errors.fullname && formik.touched.fullname && <p>{formik.errors.fullname}</p>}
+            <Input type="text" name="name" placeholder="Full Name" className="inputLogin" value={formik.values.name} onChange={formik.handleChange} />
+            {formik.errors.name && formik.touched.name && <p>{formik.errors.name}</p>}
             <Input type="email" name="email" placeholder="Email" className="inputLogin" value={formik.values.email} onChange={formik.handleChange} />
             {formik.errors.email && formik.touched.email && <p>{formik.errors.email}</p>}
             <Input type="password" name="password" placeholder="Password" className="inputLogin" value={formik.values.password} onChange={formik.handleChange} />
             {formik.errors.password && formik.touched.password && <p>{formik.errors.password}</p>}
-            <Button typr="submit" title="Sign Up" btn="login" color="blue" />
+            <Button type="submit" title="Sign Up" btn="login" color="blue" />
           </form>
           <p className={styles.acc}>
             <Input type="checkbox" className="check" name="check" /> Accept terms and condition
