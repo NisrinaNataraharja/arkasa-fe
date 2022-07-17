e588d6a16de60202ae4fdf19ce20b1ab9fc48084
Muhamad Islahuddin <el.islah.udin@gmail.com>
2022-07-17 23:28:04 +0700

    add flight detail

--- src/config/redux/reducers/rootReducer.js
+++ src/config/redux/reducers/rootReducer.js
@@ -2,12 +2,16 @@ import {combineReducers} from 'redux'
 import userReducer from './userReducer'
 import adminReducer from './adminReducer'
 import { productsReducer, detailProdReducer } from './productsReducer'
+import { ticketDetailReducer } from './ticketDetailReducer'
+import { addBookingReducer } from './addBookingReducer'
 
 const rootReducer = combineReducers({
     user: userReducer,
     admin: adminReducer,
     products: productsReducer,
-    productDetail: detailProdReducer
+    productDetail: detailProdReducer,
+    ticketDetail: ticketDetailReducer,
+    latestBooking : addBookingReducer
 })
 
 export default rootReducer
